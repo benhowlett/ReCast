8040b84e4da3eac20c2da18a0b229ae672adefc4
Ben Howlett <ben@benhowlett.com>
2022-08-09 08:29:16 -0600

    Initial Commit

---
+++ ReCast/ReCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReCastApp.swift
+//  ReCast
+//
+//  Created by Ben Howlett on 2022-08-09.
+//
+
+import SwiftUI
+
+@main
+struct ReCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
